382cb136190c69bd0e346c293ee991d5cf83ca9b
zhangqiang6 <zhangqiang6@yonyou.com>
2017-11-02 10:22:02 +0800

    初始化工程

---
+++ src/main/java/com/zworks/zombie419/Zombie419Application.java
@@ -0,0 +1,12 @@
+package com.zworks.zombie419;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zombie419Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zombie419Application.class, args);
+	}
+}
---
+++ src/test/java/com/zworks/zombie419/Zombie419ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zworks.zombie419;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Zombie419ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
